e369fdc88ea7050f66e73b66c598041825c64905
Marcin Lesiak <marcinui07@gmail.com>
2024-02-14 09:35:47 +0100

    added main app logic

--- app/src/main/java/com/example/walkazesmokiem/MainActivity.java
+++ app/src/main/java/com/example/walkazesmokiem/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.walkazesmokiem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = findViewById(R.id.editTextNumber);
+        Button button = findViewById(R.id.button);
+        Random rand = new Random();
+
+        button.setOnClickListener(view -> {
+            String test = editText.getText().toString();
+
+            if (test.isEmpty()){
+                showAlertDialog("Wprowadź liczbę aby zagrać!");
+            } else {
+                int guessedNumber = Integer.parseInt(test);
+                if (guessedNumber > 100 || guessedNumber < 0) {
+                    showAlertDialog("Wprowadź poprawną liczbę!");
+                } else {
+                    int randomNumber = rand.nextInt(100);
+                    if (guessedNumber == randomNumber) {
+                        showAlertDialog("Wygrałeś!");
+                    } else {
+                        showAlertDialog("Przegrałeś!");
+                    }
+                }
+            }
+        });
+    }
+    private void showAlertDialog(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Walka ze smokiem").setMessage(message).setPositiveButton("OK", (dialog, id) -> {});
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
